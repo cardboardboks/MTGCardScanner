60e2c490f8096687562ee250c4bb8a3ba0a20a0a
Hadley <cardboard_boks@hotmail.com>
2023-10-21 13:03:42 +1300

    Start Project

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan Card";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 350);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(282, 153);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(238, 350);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(282, 52);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(238, 82);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(540, 52);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(1443, 856);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 523);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start Camera";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(366, 552);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(26, 509);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(309, 379);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2006, 930);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using Accord;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getListCameraUSB();
+        }
+
+        private void getListCameraUSB()
+        {
+
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (filterInfoCollection.Count != 0)
+            {
+                foreach (FilterInfo filterinfo in filterInfoCollection)
+                {
+                    comboBox1.Items.Add(filterinfo.Name);
+                    comboBox1.SelectedIndex = 0;
+                    videoCaptureDevice = new VideoCaptureDevice();
+                }
+            }
+            else
+            {
+                comboBox1.Items.Add("No Cameras Found");
+            }
+
+            comboBox1.SelectedIndex = 0;
+            //
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                var totalCount = 0;
+
+                // display image in picture box  
+                pictureBox1.Image = new Bitmap(open.FileName);
+                // image file path  
+                string PicPath = open.FileName;
+
+                //OCR On currnet image
+                IronTesseract IronOcr = new IronTesseract();
+                var Result = IronOcr.Read(PicPath);
+                //Dispaly raw text
+                richTextBox1.Text = Result.Text;
+
+                // split string 
+                string text = Result.Text;                
+                string[] result = text.Split('\n');
+
+                //Format set name and numnber
+
+                //Find last feilds from OCR where set name and number are found
+                totalCount = result.Count();
+
+                string setName = result[totalCount - 1];
+                string setNum = result[totalCount - 2];
+                string setNumOut;
+                int setNumLen;
+
+                //clean up set name feild to needed numbers only
+                setName = setName.Remove(3);
+                setName = setName.ToLower();
+
+                setNum = setNum.Replace("T", "");
+                setNum = setNum.Replace("S", "");
+                setNum = setNum.Replace("L", "");
+                setNum = setNum.Replace("C", "");
+                setNum = setNum.Replace("R", "");
+                setNum = setNum.Replace("U", "");
+                setNum = setNum.Replace("M", "");
+                setNum = setNum.Replace("H", "");
+                setNum = setNum.Trim();
+
+                setNumLen = setNum.Length;                
+
+                if (setNumLen <= 3)
+                {
+                    setNum = setNum.Replace("/", "");
+                    setNum = setNum.TrimStart('0');
+                    setNumOut = setNum.Trim();
+                }
+                else if(setNumLen == 4)
+                {
+                    setNum = setNum.Replace("/", "");
+                    setNum = setNum.TrimStart('0');
+                    setNumOut = setNum.Trim();
+                }
+                else
+                {
+                    setNum = setNum.Remove(4);
+                    setNum = setNum.Replace("/", "");               
+                    setNum = setNum.TrimStart('0');
+                    setNumOut = setNum.Trim();
+                }
+
+
+                // Create an API URI
+                string[] cardID = { "https://api.scryfall.com/cards/",setName,"/",setNumOut};
+
+                string output = String.Join("", cardID);
+
+                //Display the URI
+                richTextBox2.Text = output;
+
+                //Call the URI
+                using(var client = new HttpClient())
+                {
+                    var endpoint = new Uri(output);
+                    var scrfallAPI = client.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
+                    //Debug.WriteLine(scrfallAPI);
+
+                    //Display the raw URI results
+                    richTextBox3.Text = scrfallAPI;
+
+                }
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (filterInfoCollection.Count != 0)
+            {
+                videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[comboBox1.SelectedIndex].MonikerString);
+                videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+                videoCaptureDevice.Start();
+            }
+            else
+            {
+                string message = "No Camera Selected";
+                MessageBox.Show(message);
+            }
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox2.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (filterInfoCollection.Count != 0)
+            {
+                if (videoCaptureDevice.IsRunning == true)
+                {
+                    videoCaptureDevice.Stop();
+                }
+            }
+        }
+    }
+}
+
